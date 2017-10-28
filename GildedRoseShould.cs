ce3264b975e7a2aefa21fb6a772e0bef72c6391d
Karen Xie <karen-x@outlook.com>
2017-10-29 09:41:55 +1300

    cleanup

--- GildedRoseShould.cs
+++ GildedRoseShould.cs
@@ -76,9 +76,6 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual(30, items[0].Quality);
         }
-        //        - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
-        //        Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-        //        Quality drops to 0 after the concert
 
         [Test]
         public void IncreaseQualityBy3WhenSellByDateIs5DaysOrLessForBackstagePass()
