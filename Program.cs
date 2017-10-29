e7c949f536c4d8256f3ee73388bdd1f1bf44d67c
Karen Xie <karen-x@outlook.com>
2017-10-30 09:11:59 +1300

    resharper cleanup

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+            IList<Item> items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -37,16 +37,15 @@ namespace csharp
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
-            var app = new GildedRose(Items, new ItemUpdaterFactory());
-
+            var app = new GildedRose(items, new ItemUpdaterFactory());
 
             for (var i = 0; i < 31; i++)
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (var item in items)
                 {
-                    System.Console.WriteLine(Items[j].Name + ", " + Items[j].SellIn + ", " + Items[j].Quality);
+                    Console.WriteLine($"{item.Name}, {item.SellIn}, {item.Quality}");
                 }
                 Console.WriteLine("");
                 app.UpdateItems();
