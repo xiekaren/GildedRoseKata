14bd47689a547e5f5125618473ac9388c86cd80b
Karen Xie <karen-x@outlook.com>
2017-10-30 18:31:31 +1300

    update approval test and extract items to private method

--- Program.cs
+++ Program.cs
@@ -7,35 +7,7 @@ namespace csharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("OMGHAI!");
-
-            IList<Item> items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
+            var items = GetItems();
 
             var app = new GildedRose(items, new ItemUpdaterFactory());
 
@@ -50,6 +22,37 @@ namespace csharp
                 Console.WriteLine("");
                 app.UpdateItems();
             }
+        }
+
+        private static IList<Item> GetItems()
+        {
+            return new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = SpecialItemNames.AgedBrie, SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = SpecialItemNames.Sulfuras, SellIn = 0, Quality = 80},
+                new Item {Name = SpecialItemNames.Sulfuras, SellIn = -1, Quality = 80},
+                new Item
+                {
+                    Name = SpecialItemNames.BackstagePasses,
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = SpecialItemNames.BackstagePasses,
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = SpecialItemNames.BackstagePasses,
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
         }
     }
 }
