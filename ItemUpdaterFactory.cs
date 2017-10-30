a1789476bdad6e4737f6057e84327fbb02eaca52
Karen Xie <karen-x@outlook.com>
2017-10-30 18:25:30 +1300

    add constants file

--- ItemUpdaterFactory.cs
+++ ItemUpdaterFactory.cs
@@ -2,21 +2,17 @@
 {
     public class ItemUpdaterFactory
     {
-        private const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        private const string AgedBrie = "Aged Brie";
-        private const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
-
         public ItemUpdater GetUpdaterFor(Item item)
         {
-            if (item.Name == Sulfuras)
+            if (item.Name == SpecialItemNames.Sulfuras)
             {
                 return new SulfurasItemUpdater();
             }
-            if (item.Name == AgedBrie)
+            if (item.Name == SpecialItemNames.AgedBrie)
             {
                 return new AgedBrieItemUpdater();
             }
-            if (item.Name == BackstagePasses)
+            if (item.Name == SpecialItemNames.BackstagePasses)
             {
                 return new BackstagePassesItemUpdater();
             }
---
+++ SpecialItemNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public class SpecialItemNames
+    {
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+    }
+}
